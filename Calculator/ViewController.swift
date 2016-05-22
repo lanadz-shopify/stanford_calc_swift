ddd4ce70538e9ba9e892eb0ac002d664c139b5aa
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2016-05-22 13:38:51 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Svitlana Dzyuban on 22/5/16.
+//  Copyright © 2016 Lana Dzyuban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
