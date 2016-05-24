8688d85f6f27244357f192b9f73c53d01543c124
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2016-05-24 11:55:54 +0300

    Calc from courses. Initial commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var inTheMiddleOfTyping = false
+    
+    @IBOutlet weak var display: UILabel!
+
+    
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if inTheMiddleOfTyping {
+            let currentText = display.text!
+            display.text = currentText + digit
+        } else {
+            display.text = digit
+        }
+        inTheMiddleOfTyping = true
+    }
+
+    @IBAction func performOperation(sender: UIButton) {
+        inTheMiddleOfTyping = false
+        
+        if let operation = sender.currentTitle {
+            if operation == "П" {
+                display.text = String(M_PI)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
